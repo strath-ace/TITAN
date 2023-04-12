74b8a266ac7152156892968631baf5409a3534a4
fmpmorgado <fabio.pereira-morgado@strath.ac.uk>
2023-04-12 09:50:15 +0100

    CI for Billig

---
+++ Tests/Regression/test_multi_fidelity2.py
@@ -0,0 +1,38 @@
+#
+# Copyright (c) 2023 TITAN Contributors (cf. AUTHORS.md).
+#
+# This file is part of TITAN 
+# (see https://github.com/strath-ace/TITAN).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+import pytest
+import sys
+import numpy as np
+from TITAN import main
+import os
+import meshio
+
+options, titan1 = main("Tests/Configs/multi_fidelity_2.txt")
+
+def test_cfd_files():
+	assert os.path.exists("Tests/Simulation/CFD_sol/surface_flow_0_0_cluster_1.vtk")
+
+
+TITAN_mesh1 = meshio.read("Tests/Simulation/Surface_solution/ID_1/solution_iter_000.vtk")
+TITAN_mesh2 = meshio.read("Tests/Simulation/Surface_solution/ID_2/solution_iter_000.vtk")
+SU2_mesh = meshio.read("Tests/Simulation/CFD_sol/surface_flow_0_0_cluster_1.vtk")
+
+def test_pressure():
+	assert all(np.isclose(np.round(np.sort(np.append(TITAN_mesh1.point_data["Pressure"],TITAN_mesh2.point_data["Pressure"])),2),np.round(np.sort(SU2_mesh.point_data["Pressure"].reshape(-1)),2), 0.1))
