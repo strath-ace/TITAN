8c3c479393e59239846b0afd7b55d0e742b48786
fmpmorgado <fabio.pereira-morgado@strath.ac.uk>
2023-07-21 09:49:15 +0100

    Sphere-sphere collision case and heatflux partial catalitic regression update

--- Tests/Regression/test_sphere_sphere_collision.py
+++ Tests/Regression/test_sphere_sphere_collision.py
@@ -28,14 +28,14 @@ options, titan = main("Tests/Configs/sphere-sphere.txt")
 print(titan.assembly[0].position, titan.assembly[1].position)
 
 def test_position_post_collision():
-	assert (np.round(titan.assembly[0].position[0],2) == np.round(7.07812896E+06,2))
-	assert (np.round(titan.assembly[0].position[1],5) == np.round(-7.64165177E-01,5))
-	assert (np.round(titan.assembly[0].position[2],5) == np.round(-3.29717107E+00,5))
+	assert (np.round(titan.assembly[0].position[0],2) == np.round(7.07812846E+06,2))
+	assert (np.round(titan.assembly[0].position[1],5) == np.round(-4.65305108E-01,5))
+	assert (np.round(titan.assembly[0].position[2],5) == np.round(-3.31815969,5))
 
 
-	assert (np.round(titan.assembly[1].position[0],2) == np.round(7.07812890E+06,2))
-	assert (np.round(titan.assembly[1].position[1],5) == np.round(6.32614291E-01,5))
-	assert (np.round(titan.assembly[1].position[2],5) == np.round(2.78064246E+00,5))
+	assert (np.round(titan.assembly[1].position[0],2) == np.round(7.07812911E+06,2))
+	assert (np.round(titan.assembly[1].position[1],5) == np.round(4.83193784e-01,5))
+	assert (np.round(titan.assembly[1].position[2],5) == np.round(2.79658148e+00,5))
 
 #def test_pressure(): assert np.round(np.max(titan.assembly[0].aerothermo.pressure),5) == np.round(353018.08191,5)
 #def test_heatflux(): assert np.round(np.max(titan.assembly[0].aerothermo.heatflux),5) == np.round(74730.60448,5)
