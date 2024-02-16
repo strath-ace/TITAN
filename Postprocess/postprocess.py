118c60418b94d39328807abb5c9b62c0b115ca5b
CatarinaGarbacz <ana.gomes@strath.ac.uk>
2024-02-16 22:31:02 +0000

    postprocess with save_freq hardcode. update ariane file

--- Postprocess/postprocess.py
+++ Postprocess/postprocess.py
@@ -30,7 +30,8 @@ def postprocess(options, postprocess = "wind", filter_name = None):
 
 	iter_interval = np.unique(data['Iter'].to_numpy())
 	
-	for iter_value in iter_interval:
+	#for iter_value in iter_interval:
+	for iter_value in range(0, len(iter_interval), 5):	
 		generate_visualization(options, data, iter_value, postprocess, filter_name, data_obj)
 
 def generate_visualization(options, data, iter_value, postprocess = "wind", filter_name = None, data_obj = None):
@@ -162,4 +163,4 @@ def generate_visualization(options, data, iter_value, postprocess = "wind", filt
         cells=cells,
         cell_data = cell_data)
 
-	trimesh.write(options.output_folder+'/Postprocess/'+str(iter_value).zfill(3)+'.xdmf')
+	trimesh.write(options.output_folder+'/Postprocess/'+'/solution_iter_'+str(iter_value).zfill(3)+'.xdmf')
