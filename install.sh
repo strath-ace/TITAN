576f9d5c4b554ef46cb737b06440d82fa2478201
TommyWilliamson <91695575+TommyWilliamson@users.noreply.github.com>
2025-12-01 12:15:01 +0000

    Update install.sh to check deps

--- install.sh
+++ install.sh
@@ -1,29 +1,47 @@
 #!/bin/bash
 set -eu
-echo "[INSTALL] Warning! This installation file is experimental"
-echo "[INSTALL] Welcome to TITAN!"
-echo "[INSTALL] Creating TITAN directory"
-git clone httsp://github.com/strath-ACE/TITAN -b develop
+echo "
+[INSTALL] Warning! This installation file is experimental
+"
+conda --version
+echo "
+[INSTALL] Welcome to TITAN!"
+echo "[INSTALL] Creating TITAN directory
+"
+git clone https://github.com/strath-ACE/TITAN -b develop
 cd TITAN
-echo "[INSTALL] Creating TITAN conda environment..."
+echo "
+[INSTALL] Creating TITAN conda environment...
+"
 conda env create --name titan --file titan_compatibility_env.yml
 eval "$(conda shell.bash hook)"
 conda activate titan
-echo "[INSTALL] Cloning submodules..."
+swig -version
+echo "
+[INSTALL] Cloning submodules...
+"
 git submodule update --init --recursive
 cd ./Executables
-echo "[INSTALL] Installing su2gmf..."
+echo "
+[INSTALL] Installing su2gmf...
+"
 CFLAGS="-Wno-return-mismatch -Wno-implicit-function-declaration" pip install --config-settings editable_mode=compat -e amgio/su2gmf/
 ln -s ./amgio/su2gmf/su2_to_gmf.py ./su2_to_gmf.py
 ln -s ./amgio/su2gmf/gmf_to_su2.py ./gmf_to_su2.py
-echo "[INSTALL] Intalling mutation++..."
+echo "
+[INSTALL] Intalling mutation++...
+"
 cd ./mutationpp
 python setup.py build
 python setup.py install
 conda deactivate
-echo "[INSTALL] Making PATO environment..."
+echo "
+[INSTALL] Making PATO environment...
+"
 conda config --add channels conda-forge
 conda config --add channels pato.devel
 conda config --set channel_priority strict
 conda create -y --name pato -c conda-forge -c pato.devel pato
-echo "[INSTALL] Finished, thanks for installing TITAN!"
+echo "
+[INSTALL] Finished, thanks for installing TITAN!
+"
